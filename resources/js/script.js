b3f18aa65dd5ce63f7a96dfe1848026eebc2b7f6
pjohns <patj33@protonmail.com>
2018-05-18 13:53:53 -0600

    code beautify

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,174 +1,172 @@
-
-
 $(document).ready(function() {
 
-    // For the sticky navigation
-    $('.js--section-profile').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-      offset: '60px;'
-    });
-
-
-    // For Arrows
-    function switchArrow(className) {
-      var icon = $(className + ' i');
-
-      if (icon.hasClass('ion-ios-arrow-right')) {
-          icon.addClass('ion-ios-arrow-down');
-          icon.removeClass('ion-ios-arrow-right');
-      } else {
-          icon.addClass('ion-ios-arrow-right');
-          icon.removeClass('ion-ios-arrow-down');
-      }
+  // For the sticky navigation
+  $('.js--section-profile').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
     }
-
-    document.querySelector('.js--arrow-icon-1').addEventListener('click', switchArrow.bind(this, '.js--arrow-icon-1'));
-    document.querySelector('.js--arrow-icon-2').addEventListener('click', switchArrow.bind(this, '.js--arrow-icon-2'));
-
-    $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, {
-        offset: '90%'
-    });
-
-    $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated pulse');
-    }, {
-        offset: '50%'
-    });
+  }, {
+    offset: '60px;'
+  });
 
 
+  // For Arrows
+  function switchArrow(className) {
+    var icon = $(className + ' i');
 
+    if (icon.hasClass('ion-ios-arrow-right')) {
+      icon.addClass('ion-ios-arrow-down');
+      icon.removeClass('ion-ios-arrow-right');
+    } else {
+      icon.addClass('ion-ios-arrow-right');
+      icon.removeClass('ion-ios-arrow-down');
+    }
+  }
 
+  document.querySelector('.js--arrow-icon-1').addEventListener('click', switchArrow.bind(this, '.js--arrow-icon-1'));
+  document.querySelector('.js--arrow-icon-2').addEventListener('click', switchArrow.bind(this, '.js--arrow-icon-2'));
 
-    /* Navigation scroll */
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '90%'
+  });
 
-    /* Mobile navigation */
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-
-        nav.slideToggle(200);
-
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }
-    });
-
-
-    $('.spur-poster').magnificPopup({
-        type: 'image'
-    });
-
-    $('.pipeline-poster').magnificPopup({
-        type: 'iframe'
-    });
-
-
-
-    $('.popup-gallery').magnificPopup({
-       delegate: 'a',
-       type: 'image',
-       tLoading: 'Loading image #%curr%...',
-       mainClass: 'mfp-img-mobile',
-       gallery: {
-           enabled: true,
-           navigateByImgClick: true,
-           preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-       },
-       image: {
-           tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-           titleSrc: function(item) {
-               return item.el.attr('title') + '<small>Johnson et. al https://doi.org/10.1016/j.ydbio.2018.04.014 &copy; Creative Commons</small>';
-           }
-       }
-   });
-
-
-
-
-
-    $('.popup-gallery-inverts').magnificPopup({
-       delegate: 'a',
-       type: 'image',
-       tLoading: 'Loading image #%curr%...',
-       mainClass: 'mfp-img-mobile',
-       gallery: {
-           enabled: true,
-           navigateByImgClick: true,
-           preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-       },
-       image: {
-           tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-           titleSrc: function(item) {
-               return item.el.attr('title') + '<small>&copy; Pat Johnson</small>';
-           }
-       }
-   });
-   $('.popup-gallery-thesis').magnificPopup({
-      delegate: 'a',
-      type: 'image',
-      tLoading: 'Loading image #%curr%...',
-      mainClass: 'mfp-img-mobile',
-      gallery: {
-          enabled: true,
-          navigateByImgClick: true,
-          preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-      },
-      image: {
-          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-          titleSrc: function(item) {
-              return item.el.attr('title') + '<small>&copy; Pat Johnson</small>';
-          }
-      }
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
   });
 
 
 
-   $('.magnific-gallery').each(function(index , value){
+
+
+  /* Navigation scroll */
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  /* Mobile navigation */
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  });
+
+
+  $('.spur-poster').magnificPopup({
+    type: 'image'
+  });
+
+  $('.pipeline-poster').magnificPopup({
+    type: 'iframe'
+  });
+
+
+
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>Johnson et. al https://doi.org/10.1016/j.ydbio.2018.04.014 &copy; Creative Commons</small>';
+      }
+    }
+  });
+
+
+
+
+
+  $('.popup-gallery-inverts').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>&copy; Pat Johnson</small>';
+      }
+    }
+  });
+  $('.popup-gallery-thesis').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>&copy; Pat Johnson</small>';
+      }
+    }
+  });
+
+
+
+  $('.magnific-gallery').each(function(index, value) {
     var gallery = $(this);
     var galleryImages = $(this).data('links').split(',');
-      var items = [];
-      for(var i=0;i<galleryImages.length; i++){
-        items.push({
-          src:galleryImages[i],
-          title:''
-        });
-      }
-      gallery.magnificPopup({
-        mainClass: 'mfp-fade',
-        items:items,
-        gallery:{
-          enabled:true,
-          tPrev: $(this).data('prev-text'),
-          tNext: $(this).data('next-text'),
-          preload: [0,1]
-        },
-        type: 'image'
+    var items = [];
+    for (var i = 0; i < galleryImages.length; i++) {
+      items.push({
+        src: galleryImages[i],
+        title: ''
       });
+    }
+    gallery.magnificPopup({
+      mainClass: 'mfp-fade',
+      items: items,
+      gallery: {
+        enabled: true,
+        tPrev: $(this).data('prev-text'),
+        tNext: $(this).data('next-text'),
+        preload: [0, 1]
+      },
+      type: 'image'
+    });
   });
 
 
@@ -179,7 +177,7 @@ $(document).ready(function() {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
-      if (content.style.maxHeight){
+      if (content.style.maxHeight) {
         content.style.maxHeight = null;
       } else {
         content.style.maxHeight = content.scrollHeight + "px";
