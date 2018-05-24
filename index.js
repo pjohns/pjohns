73425cf10486a68340172888291988beaf60c865
pjohns <patj33@protonmail.com>
2018-05-24 17:58:49 -0600

    use of parcel

---
+++ index.js
@@ -0,0 +1,6 @@
+window.jQuery = window.$ = require("jquery");
+require('waypoints/lib/jquery.waypoints');
+require('lazysizes');
+require('magnific-popup');
+require('html5shiv');
+require('./resources/js/script')
