0c4827edf5e2cf66db33ffa4681788899d661b96
pjohns <patj33@protonmail.com>
2018-05-25 13:15:00 -0600

    inline critical css

---
+++ critical.js
@@ -0,0 +1,11 @@
+var critical = require('critical');
+
+critical.generate({
+  inline: true,
+  base: 'dist/',
+  src: 'index.html',
+  dest: 'index.html',
+  ignore: ['@font-face'],
+  minify: true,
+  extract: true
+});
